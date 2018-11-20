8c2f4b1098854cee6d618fe3884c4b7f2d23f1fb
Alexandr <Alexandr@LVIV.VISARD.COM>
2018-11-20 11:23:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alexandr/mytestgit1/MainActivityTestGit1.kt
@@ -0,0 +1,12 @@
+package com.example.alexandr.mytestgit1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTestGit1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_test_git1)
+    }
+}
